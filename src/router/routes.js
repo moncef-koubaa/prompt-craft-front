eab62d6e306f695160b390369ca0af4876f7436f
Moncef Koubaa <moncefkoubaa26@gmail.com>
2025-05-05 13:12:13 +0100

    feat : chat-bot page

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,5 +3,15 @@ export default [
         path : '/',
         name : 'home',
         component : () => import('@/views/Home.vue'),
+    },
+    {
+        path : '/bids-page',
+        name : 'bids-page',
+        component : () => import('@/views/bids-page.vue'),
+    },
+    {
+        path: '/chat-bot',
+        name: 'chat-bot',
+        component: () => import('@/views/chat-bot.vue'),
     }
 ]
