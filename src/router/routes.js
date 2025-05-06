dfeb774847d5a70c0d3132d74efadfc574b955b4
Amir-Mallek <amir.mallek@insat.ucar.tn>
2025-05-06 14:54:34 +0100

    feat: add my-nft route and view

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,5 +3,10 @@ export default [
         path : '/',
         name : 'home',
         component : () => import('@/views/Home.vue'),
+    },
+    {
+        path : '/my-nft',
+        name : 'my-nft',
+        component : () => import('@/views/my-nft.vue'),
     }
 ]
